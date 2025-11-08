43fbe81f2a1db38573aaffc5c398f9a636c4ff2a
do4mother <singgihputrap@gmail.com>
2025-11-08 13:01:51 +0700

    style: format code for consistency and readability

--- apps/backend/src/db/schema.ts
+++ apps/backend/src/db/schema.ts
@@ -1,21 +1,23 @@
-import { relations } from 'drizzle-orm';
-import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
-import { timestampColumn } from './column.helper';
+import { relations } from "drizzle-orm";
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { timestampColumn } from "./column.helper";
 
 export const users = sqliteTable("users", {
-  id: int().primaryKey(),
-  ...timestampColumn,
-  email: int().notNull().unique(),
-})
+	id: int().primaryKey(),
+	...timestampColumn,
+	email: int().notNull().unique(),
+});
 
 export const user_providers = sqliteTable("user_providers", {
-  id: int().primaryKey(),
-  ...timestampColumn,
-  user_id: int().notNull().references(() => users.id),
-  provider: text().notNull(),
-  provider_user_id: text().notNull(),
-})
+	id: int().primaryKey(),
+	...timestampColumn,
+	user_id: int()
+		.notNull()
+		.references(() => users.id),
+	provider: text().notNull(),
+	provider_user_id: text().notNull(),
+});
 
 export const userProviderRelations = relations(users, ({ many }) => ({
-  providers: many(user_providers)
-}))
+	providers: many(user_providers),
+}));
