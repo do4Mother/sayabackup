007fd838a8a03ca5c6c24b903bc67044248e90c0
do4mother <singgihputrap@gmail.com>
2025-11-08 13:58:40 +0700

    feat: refactor RootLayout to include user authentication and loading state handling

--- apps/frontend/app/_layout.tsx
+++ apps/frontend/app/_layout.tsx
@@ -1,13 +1,46 @@
-import { client, queryClient, TRPCProvider } from "@/trpc/trpc";
+import { useApp } from "@/hooks/use_app";
+import { client, queryClient, trpc, TRPCProvider } from "@/trpc/trpc";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
+import { ActivityIndicator, View } from "react-native";
 
 export default function RootLayout() {
   return (
     <TRPCProvider client={client} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
-        <Stack screenOptions={{ headerShown: false }} />
+        <StackRootLayout />
       </QueryClientProvider>
     </TRPCProvider>
+  );
+}
+
+function StackRootLayout() {
+  const user = trpc.auth.me.useQuery();
+  const router = useRouter();
+  const appState = useApp((state) => state.user);
+  const setUser = useApp((state) => state.setUser);
+
+  useEffect(() => {
+    if (user.data) {
+      setUser(user.data.user);
+      router.replace("/home");
+    }
+  }, [user.data]);
+
+  if (user.isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size={"large"} />
+      </View>
+    );
+  }
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Protected guard={appState != null}>
+        <Stack.Screen name="(protected)" />
+      </Stack.Protected>
+    </Stack>
   );
 }
