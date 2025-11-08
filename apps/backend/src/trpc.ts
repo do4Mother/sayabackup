6effb83e70b5bce7e816f6e97407980f6d031166
do4mother <singgihputrap@gmail.com>
2025-11-08 10:59:00 +0700

    feat: add DrizzleD1Database to context for enhanced database access

--- apps/backend/src/trpc.ts
+++ apps/backend/src/trpc.ts
@@ -1,4 +1,5 @@
 import { initTRPC } from '@trpc/server'
+import { DrizzleD1Database } from 'drizzle-orm/d1'
 
 
 type SetCookieOptions = {
@@ -11,7 +12,8 @@ type SetCookieOptions = {
 export type Context = {
   env: Env,
   setCookie: (name: string, value: string, options?: SetCookieOptions) => void,
-  getCookie: (name: string) => string | null
+  getCookie: (name: string) => string | null,
+  db: DrizzleD1Database
 }
 
 export const t = initTRPC.context<Context>().create()
