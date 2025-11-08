bcb4e09b55e4c5eb936442f0ea35dd379e9d65ad
do4mother <singgihputrap@gmail.com>
2025-11-08 13:58:33 +0700

    feat: simplify index component by removing unused Pressable and console.log

--- apps/frontend/app/index.tsx
+++ apps/frontend/app/index.tsx
@@ -1,10 +1,9 @@
+import GoogleSignInButton from "@/components/buttons/GoogleSignInButton";
 import { trpc } from "@/trpc/trpc";
-import { Pressable, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   const { data } = trpc.hello.useQuery();
-
-  console.log("data", data);
 
   return (
     <View
@@ -17,23 +16,12 @@ export default function Index() {
     >
       <Text style={{ fontSize: 28, fontWeight: 800 }}>SayaBackup</Text>
       <Text style={{ fontSize: 20, textAlign: "center", marginTop: 10 }}>
-        Your personal backup solution for photos and videos.
+        Your personal backup solution for photos and videos. {data}
       </Text>
 
-      <Pressable
-        style={{
-          marginTop: 50,
-          paddingHorizontal: 20,
-          paddingVertical: 15,
-          backgroundColor: "#4285F4",
-          borderRadius: 5,
-          width: "100%",
-        }}
-      >
-        <Text style={{ color: "#fff" }}>Sign in with Google</Text>
-      </Pressable>
-
-      <div>Hello World</div>
+      <View style={{ marginTop: 80 }}>
+        <GoogleSignInButton />
+      </View>
     </View>
   );
 }
