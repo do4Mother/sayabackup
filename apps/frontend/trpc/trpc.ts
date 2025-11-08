855cb88dc7564768ebd5867f247772db14001fb9
do4mother <singgihputrap@gmail.com>
2025-11-08 13:59:11 +0700

    feat: enhance queryClient configuration with error handling and retry logic

--- apps/frontend/trpc/trpc.ts
+++ apps/frontend/trpc/trpc.ts
@@ -1,11 +1,38 @@
 import { QueryClient } from "@tanstack/react-query";
-import { httpBatchLink } from "@trpc/client";
+import { httpBatchLink, TRPCClientError } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
+import { z } from "zod";
 import { AppRouter } from "../../backend/src/routers/routers";
 
 export const trpc = createTRPCReact<AppRouter>();
 export const TRPCProvider = trpc.Provider;
-export const queryClient = new QueryClient();
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry(failureCount, error) {
+        if (error instanceof TRPCClientError) {
+          const errorData = z
+            .object({ code: z.string(), httpStatus: z.number() })
+            .safeParse(error.data);
+
+          if (errorData.success) {
+            // Do not retry for 4xx errors
+            if (
+              errorData.data.httpStatus >= 400 &&
+              errorData.data.httpStatus < 500
+            ) {
+              return false;
+            }
+          }
+        }
+
+        return failureCount < 3;
+      },
+    },
+  },
+});
+
 export const client = trpc.createClient({
   links: [
     httpBatchLink({
