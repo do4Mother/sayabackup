2a548a0e49c88ee3bfa5f9d0bf70b9f068369cc7
do4mother <singgihputrap@gmail.com>
2025-11-11 22:41:09 +0700

    feat: update user DTO to remove optional key and mask value based on masking prop

--- apps/backend/src/routers/auth/dto/user.dto.ts
+++ apps/backend/src/routers/auth/dto/user.dto.ts
@@ -4,8 +4,5 @@ export const userDto = (props: { masking: boolean } = { masking: false }) =>
 	z.object({
 		id: z.number(),
 		email: z.email(),
-		key: z
-			.string()
-			.optional()
-			.transform((v) => (props.masking ? undefined : v)),
+		key: z.string().transform((v) => (props.masking ? "" : v)),
 	});
