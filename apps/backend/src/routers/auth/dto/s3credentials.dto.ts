0b2317e88566087a84235af02077db52888eb51b
do4mother <singgihputrap@gmail.com>
2025-11-11 22:41:00 +0700

    feat: refactor s3CredentialsDto to accept masking option for secret access key

--- apps/backend/src/routers/auth/dto/s3credentials.dto.ts
+++ apps/backend/src/routers/auth/dto/s3credentials.dto.ts
@@ -1,9 +1,15 @@
 import z from "zod";
 
-export const s3CredentialsDto = z.object({
-	bucket_name: z.string().min(3).max(63),
-	region: z.string().min(2).max(100),
-	access_key_id: z.string().min(1),
-	secret_access_key: z.string().min(1),
-	endpoint: z.string().min(1),
-});
+export const s3CredentialsDto = (
+	props: { masking: boolean } = { masking: false },
+) =>
+	z.object({
+		bucket_name: z.string().min(3).max(63),
+		region: z.string().min(2).max(100),
+		access_key_id: z.string().min(1),
+		secret_access_key: z
+			.string()
+			.min(1)
+			.transform((v) => (props.masking ? "" : v)),
+		endpoint: z.string().min(1),
+	});
