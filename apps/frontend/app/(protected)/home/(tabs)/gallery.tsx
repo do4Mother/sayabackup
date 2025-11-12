4852afb620923179023f58cadc47049e1de97d65
do4mother <singgihputrap@gmail.com>
2025-11-12 21:28:40 +0700

    refactor: simplify gallery layout by removing ScrollView and adjusting styles

--- apps/frontend/app/(protected)/home/(tabs)/gallery.tsx
+++ apps/frontend/app/(protected)/home/(tabs)/gallery.tsx
@@ -1,34 +1,31 @@
 import Heading from "@/components/app/Heading";
 import FloatingActionButton from "@/components/buttons/FloatingActionButton";
 import { FontAwesome6 } from "@expo/vector-icons";
-import { FlatList, Image, ScrollView, View } from "react-native";
+import { Dimensions, FlatList, Image, View } from "react-native";
 
 export default function HomeTabPage() {
+  const imageWidth = Dimensions.get("window").width / 4 - 16; // 4 columns with some spacing
+
   return (
     <View className="bg-background">
       <FloatingActionButton
         label="Upload"
         icon={<FontAwesome6 name="upload" size={16} />}
       />
-      <ScrollView>
-        <Heading title="Gallery" />
-        <FlatList
-          data={Array.from({ length: 200 }, (_, i) => ({ key: i.toString() }))}
-          numColumns={4}
-          contentContainerStyle={{
-            gap: 8,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-          columnWrapperStyle={{ gap: 4 }}
-          renderItem={({ item }) => (
-            <Image
-              source={{ uri: `https://picsum.photos/id/${item.key}/200/200` }}
-              style={{ width: 100, height: 100, borderRadius: 8 }}
-            />
-          )}
-        />
-      </ScrollView>
+      <Heading title="Gallery" />
+      <FlatList
+        data={Array.from({ length: 200 }, (_, i) => ({ key: i.toString() }))}
+        numColumns={4}
+        contentContainerClassName="items-center gap-y-4 justify-center"
+        columnWrapperClassName="gap-2"
+        renderItem={({ item }) => (
+          <Image
+            source={{ uri: `https://picsum.photos/id/${item.key}/200/200` }}
+            className="aspect-square rounded-lg"
+            style={{ width: imageWidth }}
+          />
+        )}
+      />
     </View>
   );
 }
