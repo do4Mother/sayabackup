d51261318bb61fbbcce6c0b48d3feedb5fd9eace
do4mother <singgihputrap@gmail.com>
2025-11-19 23:33:00 +0700

    feat: enhance routing and user state management in layout components

--- apps/frontend/app/(protected)/_layout.tsx
+++ apps/frontend/app/(protected)/_layout.tsx
@@ -1,8 +1,11 @@
 import Header from "@/components/app/Header";
+import { useApp } from "@/hooks/use_app";
 import { Stack } from "expo-router";
 import React from "react";
 
 export default function ProtectedLayout() {
+  const userState = useApp((state) => state.user);
+
   return (
     <Stack
       screenOptions={{
@@ -11,6 +14,10 @@ export default function ProtectedLayout() {
           <Header title={props.options.title ?? props.route.name} />
         ),
       }}
-    />
+    >
+      <Stack.Protected guard={userState != null}>
+        <Stack.Screen name="(protected" />
+      </Stack.Protected>
+    </Stack>
   );
 }
--- apps/frontend/app/_layout.tsx
+++ apps/frontend/app/_layout.tsx
@@ -3,7 +3,7 @@ import { useApp } from "@/hooks/use_app";
 import { client, queryClient, trpc, TRPCProvider } from "@/trpc/trpc";
 import { PortalHost } from "@rn-primitives/portal";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Stack, useRouter } from "expo-router";
+import { Stack, usePathname, useRouter } from "expo-router";
 import { useEffect } from "react";
 import { ActivityIndicator, View } from "react-native";
 
@@ -20,14 +20,18 @@ export default function RootLayout() {
 
 function StackRootLayout() {
   const user = trpc.auth.me.useQuery();
-  const router = useRouter();
-  const appState = useApp((state) => state.user);
   const setUser = useApp((state) => state.setUser);
+  const router = useRouter();
+  const pathname = usePathname();
+
+  console.log("Current pathname:", pathname);
 
   useEffect(() => {
     if (user.data) {
       setUser(user.data.user);
-      router.replace("/home/gallery");
+      if (pathname === "/") {
+        router.replace("/(protected)/home/(tabs)/gallery");
+      }
     }
   }, [user.data]);
 
@@ -44,10 +48,6 @@ function StackRootLayout() {
       screenOptions={{
         headerShown: false,
       }}
-    >
-      <Stack.Protected guard={appState != null}>
-        <Stack.Screen name="(protected)" />
-      </Stack.Protected>
-    </Stack>
+    />
   );
 }
