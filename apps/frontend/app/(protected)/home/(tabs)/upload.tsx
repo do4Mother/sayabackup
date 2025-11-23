fa072c21771a6c5e61c4b75fd52c89faffa07826
do4mother <singgihputrap@gmail.com>
2025-11-23 20:55:06 +0700

    feat: add unique ID generation for media items and improve upload handling

--- apps/frontend/app/(protected)/home/(tabs)/upload.tsx
+++ apps/frontend/app/(protected)/home/(tabs)/upload.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/app/Header";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
@@ -5,14 +6,17 @@ import { Text } from "@/components/ui/text";
 import { formatFileSize } from "@/lib/file_size";
 import { trpc } from "@/trpc/trpc";
 import { Ionicons } from "@expo/vector-icons";
+import { randomString } from "@sayabackup/utils";
 import axios, { CanceledError } from "axios";
 import { ImageManipulator } from "expo-image-manipulator";
 import * as ImagePicker from "expo-image-picker";
+import { Stack } from "expo-router";
 import { useState } from "react";
 import { FlatList, Image, Platform, View } from "react-native";
 import { match } from "ts-pattern";
 
 type Media = {
+  id: string;
   file: File;
   uri: string;
   thumbnail: Blob;
@@ -83,6 +87,7 @@ export default function UploadTabpage() {
           }
 
           return {
+            id: randomString(12),
             file: asset.file!,
             uri: uri,
             thumbnail: thumbnailBlob,
@@ -134,7 +139,7 @@ export default function UploadTabpage() {
               const processedBytes = progressEvent.loaded;
               setMedia((prevMedia) =>
                 prevMedia.map((m) =>
-                  m.uri === media.uri ? { ...m, processedBytes } : m,
+                  m.id === media.id ? { ...m, processedBytes } : m,
                 ),
               );
 
@@ -163,81 +168,107 @@ export default function UploadTabpage() {
   const onCancelUpload = (mediaItem: Media) => {
     mediaItem.abortController?.abort();
 
-    setMedia((prevMedia) => prevMedia.filter((m) => m.uri !== mediaItem.uri));
+    setMedia((prevMedia) => prevMedia.filter((m) => m.id !== mediaItem.id));
+  };
+
+  const onClearAll = () => {
+    for (const mediaItem of media) {
+      mediaItem.abortController?.abort();
+    }
+    setMedia([]);
   };
 
   return (
-    <View className="bg-background flex-1">
-      {match(media.length === 0)
-        .with(true, () => (
-          <View className="flex-1 items-center justify-center gap-y-4">
-            <Text className="font-medium text-lg">Share Your Moments ✨</Text>
-            <Button onPress={pickMedia}>
-              <Ionicons name="cloud-upload" size={22} color="white" />
-              <Text>Choose file</Text>
-            </Button>
-          </View>
-        ))
-        .otherwise(() => (
-          <>
-            <View className="items-end px-4">
-              <Button
-                className="gap-x-1"
-                variant={"ghost"}
-                onPress={() => {
-                  setMedia([]);
-                }}
-              >
-                <Ionicons name="close" size={22} className="mt-0.5" />
-                <Text className="leading-none">Clear</Text>
+    <>
+      <Stack.Screen
+        options={{
+          title: "Upload",
+          header: (props) => (
+            <Header
+              title={props.options.title ?? ""}
+              disableBackButton
+              variant="large"
+              action={match(media.length === 0)
+                .with(false, () => (
+                  <Button variant="ghost" size={"icon"} onPress={pickMedia}>
+                    <Ionicons name="add" size={22} />
+                  </Button>
+                ))
+                .otherwise(() => null)}
+            />
+          ),
+        }}
+      />
+      <View className="bg-background flex-1">
+        {match(media.length === 0)
+          .with(true, () => (
+            <View className="flex-1 items-center justify-center gap-y-4">
+              <Text className="font-medium text-lg">Share Your Moments ✨</Text>
+              <Button onPress={pickMedia}>
+                <Ionicons name="cloud-upload" size={22} color="white" />
+                <Text>Choose file</Text>
               </Button>
             </View>
+          ))
+          .otherwise(() => (
+            <>
+              <View className="items-end px-4">
+                <Button
+                  className="gap-x-1"
+                  variant={"ghost"}
+                  onPress={onClearAll}
+                >
+                  <Ionicons name="close" size={22} className="mt-0.5" />
+                  <Text className="leading-none">Clear</Text>
+                </Button>
+              </View>
 
-            <FlatList
-              data={media}
-              contentContainerClassName="gap-4 px-4"
-              renderItem={({ item }) => (
-                <Card className="gap-4 flex-row px-4">
-                  <Image
-                    source={{
-                      uri: item.uri,
-                    }}
-                    className="size-20 rounded-md"
-                  />
-                  <View className="flex-1">
-                    <CardHeader className="px-0 flex-1 flex-row">
-                      <CardTitle className="leading-5 flex-1 break-all">
-                        {item.name}
-                      </CardTitle>
-                      <Button
-                        size="icon"
-                        variant={"ghost"}
-                        onPress={() => onCancelUpload(item)}
-                      >
-                        <Ionicons
-                          name="trash"
-                          className="text-red-500"
-                          size={18}
+              <FlatList
+                data={media}
+                contentContainerClassName="gap-4 px-4"
+                renderItem={({ item }) => (
+                  <Card className="gap-4 flex-row px-4">
+                    <Image
+                      source={{
+                        uri: item.uri,
+                      }}
+                      className="size-20 rounded-md"
+                    />
+                    <View className="flex-1">
+                      <CardHeader className="px-0 flex-1 flex-row">
+                        <CardTitle className="leading-5 flex-1 break-all">
+                          {item.name}
+                        </CardTitle>
+                        <Button
+                          size="icon"
+                          variant={"ghost"}
+                          onPress={() => onCancelUpload(item)}
+                        >
+                          <Ionicons
+                            name="trash"
+                            className="text-red-500"
+                            size={18}
+                          />
+                        </Button>
+                      </CardHeader>
+                      <CardContent className="px-0 mt-2">
+                        <Progress
+                          value={(item.processedBytes / item.size) * 100}
+                          className="my-0"
                         />
-                      </Button>
-                    </CardHeader>
-                    <CardContent className="px-0 mt-2">
-                      <Progress
-                        value={(item.processedBytes / item.size) * 100}
-                        className="my-0"
-                      />
-                      <Text className="mt-1 text-sm">
-                        {((item.processedBytes / item.size) * 100).toFixed(0)}%
-                        ({formatFileSize(item.processedBytes)} of{" "}
-                        {formatFileSize(item.size)})
-                      </Text>
-                    </CardContent>
-                  </View>
-                </Card>
-              )}
-            />
-          </>
-        ))}
-    </View>
+                        <Text className="mt-1 text-sm">
+                          {((item.processedBytes / item.size) * 100).toFixed(0)}
+                          % ({formatFileSize(item.processedBytes)} of{" "}
+                          {formatFileSize(item.size)})
+                        </Text>
+                      </CardContent>
+                    </View>
+                  </Card>
+                )}
+              />
+            </>
+          ))}
+      </View>
+    </>
   );
 }
