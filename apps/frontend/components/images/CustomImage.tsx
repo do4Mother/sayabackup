db909756dd06b5708f9dc5b680a9c11b202a9cff
do4mother <singgihputrap@gmail.com>
2025-11-29 13:59:55 +0700

    feat: add CustomImage component for dynamic image retrieval from S3

---
+++ apps/frontend/components/images/CustomImage.tsx
@@ -0,0 +1,34 @@
+import { getImage } from "@/s3/get_image";
+import { trpc } from "@/trpc/trpc";
+import { useQuery } from "@tanstack/react-query";
+import { Image } from "expo-image";
+import { ComponentProps } from "react";
+
+export default function CustomImage(props: ComponentProps<typeof Image>) {
+  if (
+    !props.source ||
+    typeof props.source !== "object" ||
+    !("uri" in props.source)
+  ) {
+    throw new Error("Invalid image source");
+  }
+
+  const clientUtils = trpc.useUtils();
+  const key = clientUtils.auth.me.getData()?.user.key ?? "";
+
+  const source = props.source;
+
+  const image = useQuery({
+    queryKey: ["custom-image", props.source],
+    queryFn: async () => {
+      const response = await getImage({
+        path: source.uri ?? "",
+        key: key,
+      });
+
+      return response;
+    },
+  });
+
+  return <Image {...props} source={{ uri: image.data?.url }} />;
+}
