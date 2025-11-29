a239f76eeb5c2987f4b0a4ba6b9c0785a5eacad8
do4mother <singgihputrap@gmail.com>
2025-11-29 15:18:50 +0700

    feat: update selectedImages type to use Image for better type safety

--- apps/frontend/hooks/use_select_image.ts
+++ apps/frontend/hooks/use_select_image.ts
@@ -1,18 +1,21 @@
 import { create } from "zustand";
+import { AppRouterOutput } from "../../backend/src/routers/routers";
+
+type Image = AppRouterOutput["gallery"]["get"]["items"][number];
 
 type State = {
-  selectedImages: string[];
+  selectedImages: Image[];
 };
 
 type StateAction = {
-  setSelectedImages: (images: string[]) => void;
+  setSelectedImages: (images: Image[]) => void;
 };
 
 type UseSelectedImage = State & StateAction;
 
 export const useSelectedImage = create<UseSelectedImage>((set) => ({
   selectedImages: [],
-  setSelectedImages: (images: string[]) =>
+  setSelectedImages: (images: Image[]) =>
     set(() => ({
       selectedImages: images,
     })),
