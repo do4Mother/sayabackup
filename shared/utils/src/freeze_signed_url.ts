64c36f45aa82205241a39adbbcaf2e853e80fd01
do4mother <singgihputrap@gmail.com>
2025-11-29 13:57:47 +0700

    feat: add freezeSignedUrl utility function for caching signed URLs

---
+++ shared/utils/src/freeze_signed_url.ts
@@ -0,0 +1,25 @@
+const inMemoryCache = new Map<string, { url: string; expires: number }>();
+
+/**
+ * Freezes regenerate signed URL by checking inMemoryCache
+ */
+export async function freezeSignedUrl(data: {
+  url: string;
+  ttlSeconds: number;
+  newUrlCallback?: () => string | Promise<string>;
+}): Promise<string> {
+  const now = Date.now();
+  const cacheEntry = inMemoryCache.get(data.url);
+
+  if (cacheEntry && cacheEntry.expires > now) {
+    return cacheEntry.url;
+  }
+
+  const expires = now + data.ttlSeconds * 1000;
+  const newUrl = await Promise.resolve(
+    data.newUrlCallback ? data.newUrlCallback() : data.url,
+  );
+  inMemoryCache.set(data.url, { url: newUrl, expires });
+
+  return newUrl;
+}
--- shared/utils/src/index.ts
+++ shared/utils/src/index.ts
@@ -1,5 +1,6 @@
 // Export all utility functions from this file
 
 export * from './crpyto';
+export * from "./freeze_signed_url";
 export * from './random-string';
 
