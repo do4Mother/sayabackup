cc4abf4697e88316f9dd6dbccdbd2b54c84d861b
do4mother <singgihputrap@gmail.com>
2025-11-30 00:12:17 +0700

    feat: remove S3-related functionality and dependencies from auth router

--- apps/backend/src/routers/auth/index.ts
+++ apps/backend/src/routers/auth/index.ts
@@ -1,14 +1,10 @@
 import { router } from "../../trpc";
-import { decryptS3 } from "./decrypt_s3";
 import { google } from "./google";
 import { logout } from "./logout";
 import { me } from "./me";
-import { s3credentials } from "./s3credentials";
 
 export const authRouter = router({
 	google,
 	me,
-	s3credentials,
-	decryptS3,
 	logout,
 });
