d574de2a07733cf889bf5455943b3560414826ae
do4mother <singgihputrap@gmail.com>
2025-11-29 23:55:46 +0700

    feat: refactor create gallery mutation to remove S3 dependency and use input mime type

--- apps/backend/src/routers/gallery/create.ts
+++ apps/backend/src/routers/gallery/create.ts
@@ -1,30 +1,15 @@
-import { GetObjectCommand } from "@aws-sdk/client-s3";
 import { gallery } from "../../db/schema";
-import { protectedWithS3 } from "../../middlewares/protected-with-s3";
-import { createS3Client } from "../../utils/s3_client";
+import { protectedProcdure } from "../../middlewares/protected";
 import { createGalleryDto } from "./dto/create.dto";
 
-export const create = protectedWithS3
+export const create = protectedProcdure
 	.input(createGalleryDto)
 	.mutation(async ({ ctx, input }) => {
-		// get mime type
-		const client = createS3Client(ctx.s3credentials);
-
-		const object = await client
-			.send(
-				new GetObjectCommand({
-					Bucket: ctx.s3credentials.bucket_name,
-					Key: input.filePath,
-				}),
-			)
-			// if unable to get, fallback to input mime type
-			.catch(() => ({ ContentType: input.mimeType }));
-
 		await ctx.db.insert(gallery).values({
 			file_path: input.filePath,
 			thumbnail_path: input.thumbnailPath,
 			user_id: ctx.user.id,
 			album_id: input.albumId,
-			mime_type: object.ContentType ?? "",
+			mime_type: input.mimeType,
 		});
 	});
