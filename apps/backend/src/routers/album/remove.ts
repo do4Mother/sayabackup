c018be0a86da43a02860ea582885b00174b7fc25
do4mother <singgihputrap@gmail.com>
2025-11-30 00:12:23 +0700

    feat: refactor remove mutation to eliminate S3 dependency and simplify input handling

--- apps/backend/src/routers/album/remove.ts
+++ apps/backend/src/routers/album/remove.ts
@@ -1,16 +1,13 @@
-import { DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { TRPCError } from "@trpc/server";
 import { and, eq, isNull } from "drizzle-orm";
 import z from "zod";
-import { albums, gallery } from "../../db/schema";
-import { protectedWithS3 } from "../../middlewares/protected-with-s3";
-import { createS3Client } from "../../utils/s3_client";
+import { albums } from "../../db/schema";
+import { protectedProcdure } from "../../middlewares/protected";
 
-export const remove = protectedWithS3
+export const remove = protectedProcdure
 	.input(
 		z.object({
 			id: z.string(),
-			withImages: z.boolean().nullish(),
 		}),
 	)
 	.mutation(async ({ ctx, input }) => {
@@ -30,46 +27,6 @@ export const remove = protectedWithS3
 				code: "NOT_FOUND",
 				message: "Album not found",
 			});
-		}
-
-		if (input.withImages) {
-			const images = await ctx.db
-				.select()
-				.from(gallery)
-				.where(
-					and(
-						eq(gallery.album_id, album.id),
-						isNull(gallery.deleted_at),
-						eq(gallery.user_id, ctx.user.id),
-					),
-				);
-
-			const client = createS3Client(ctx.s3credentials);
-
-			for (const image of images) {
-				await client.send(
-					new DeleteObjectCommand({
-						Bucket: ctx.s3credentials.bucket_name,
-						Key: image.file_path,
-					}),
-				);
-
-				await client.send(
-					new DeleteObjectCommand({
-						Bucket: ctx.s3credentials.bucket_name,
-						Key: image.thumbnail_path,
-					}),
-				);
-
-				await ctx.db
-					.update(gallery)
-					.set({
-						deleted_at: Date.now(),
-					})
-					.where(
-						and(eq(gallery.id, image.id), eq(gallery.user_id, ctx.user.id)),
-					);
-			}
 		}
 
 		await ctx.db
