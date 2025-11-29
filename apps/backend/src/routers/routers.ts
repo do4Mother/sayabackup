2438d793692c0bd478a1c1284acc1e4cec4fc29e
do4mother <singgihputrap@gmail.com>
2025-11-29 23:44:50 +0700

    feat: remove S3 router and related upload functionality

--- apps/backend/src/routers/routers.ts
+++ apps/backend/src/routers/routers.ts
@@ -3,11 +3,9 @@ import { router } from "../trpc";
 import { albumRouter } from "./album";
 import { authRouter } from "./auth";
 import { galleryRouter } from "./gallery";
-import { s3Router } from "./s3";
 
 const appRouter = router({
 	auth: authRouter,
-	s3: s3Router,
 	gallery: galleryRouter,
 	album: albumRouter,
 });
--- apps/backend/src/routers/s3/index.ts
+++
@@ -1,6 +0,0 @@
-import { router } from "../../trpc";
-import { upload } from "./upload";
-
-export const s3Router = router({
-	upload,
-});
