0ce4a74a3539ac3c2436ffc03c7220a43fd6e60c
do4mother <singgihputrap@gmail.com>
2025-12-07 21:16:14 +0700

    feat: refactor ProtectedLayout to improve structure and add logo display

--- apps/frontend/app/(protected)/_layout.tsx
+++ apps/frontend/app/(protected)/_layout.tsx
@@ -1,6 +1,9 @@
+import Logo from "@/assets/images/sayabackup.png";
 import { useApp } from "@/hooks/use_app";
-import { Redirect, Stack } from "expo-router";
+import { Image } from "expo-image";
+import { Redirect, Stack, useRouter } from "expo-router";
 import React from "react";
+import { Pressable, View } from "react-native";
 
 // Configure anchor for modal routes
 export const unstable_settings = {
@@ -9,24 +12,34 @@ export const unstable_settings = {
 
 export default function ProtectedLayout() {
   const userState = useApp((state) => state.user);
+  const router = useRouter();
 
   if (!userState) {
     return <Redirect href="/" />;
   }
 
   return (
-    <Stack
-      screenOptions={{
-        headerShadowVisible: false,
-      }}
-    >
-      {/* Configure modal presentation for gallery route */}
-      <Stack.Screen
-        name="gallery/index"
-        options={{
-          presentation: "modal",
-        }}
-      />
-    </Stack>
+    <View className="flex-1">
+      <View className="p-4 hidden md:flex bg-background border-border border-b">
+        <Pressable onPress={() => router.push("/home/gallery")}>
+          <Image source={Logo} style={{ width: 180, height: 40 }} />
+        </Pressable>
+      </View>
+      <View className="flex-1">
+        <Stack
+          screenOptions={{
+            headerShadowVisible: false,
+          }}
+        >
+          {/* Configure modal presentation for gallery route */}
+          <Stack.Screen
+            name="gallery/index"
+            options={{
+              presentation: "modal",
+            }}
+          />
+        </Stack>
+      </View>
+    </View>
   );
 }
