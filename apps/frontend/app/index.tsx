c2fe8415440dcabc35020dab8cf6be5178ab15ba
do4mother <singgihputrap@gmail.com>
2026-03-05 21:51:45 +0700

    feat: enhance Tailwind CSS configuration and add uniwind support

--- apps/frontend/app/index.tsx
+++ apps/frontend/app/index.tsx
@@ -1,8 +1,10 @@
-import { Text, View } from 'react-native';
-import "./global.css";
+import { Text, View } from "react-native";
+import "../global.css";
 
 export default function App() {
-  return <View>
-    <Text>Welcome to the React Native app!</Text>
-  </View>
+	return (
+		<View className="bg-black flex-1 justify-center items-center">
+			<Text className="text-white">Welcome to the React Native app!</Text>
+		</View>
+	);
 }
