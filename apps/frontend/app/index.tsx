9d4e47374aed283e6960db6e2c78b4c5a3108091
do4mother <singgihputrap@gmail.com>
2026-03-05 21:40:28 +0700

    chore: remove redundant code blocks and clean up change logs

---
+++ apps/frontend/app/index.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from 'react-native';
+import "./global.css";
+
+export default function App() {
+  return <View>
+    <Text>Welcome to the React Native app!</Text>
+  </View>
+}
