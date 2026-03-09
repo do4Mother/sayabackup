a9ad3f321e8e2190bec07ba272ee7fec9923f89c
do4mother <singgihputrap@gmail.com>
2026-03-09 16:11:22 +0700

    feat: add CustomImage component for optimized image loading with lazy behavior

---
+++ apps/frontend/components/app/CustomImage.tsx
@@ -0,0 +1,35 @@
+import { getFile } from "@/s3/get_file";
+import { trpc } from "@/trpc/trpc";
+import { useQuery } from "@tanstack/react-query";
+import { Image } from "expo-image";
+import { ComponentProps } from "react";
+
+export default function CustomImage(props: ComponentProps<typeof Image>) {
+	if (
+		!props.source ||
+		typeof props.source !== "object" ||
+		!("uri" in props.source)
+	) {
+		throw new Error("Invalid image source");
+	}
+
+	const user = trpc.auth.me.useQuery();
+	const key = user.data?.user.key ?? "";
+
+	const source = props.source;
+
+	const image = useQuery({
+		enabled: !!source.uri,
+		queryKey: ["custom-image", props.source],
+		queryFn: async () => {
+			const response = await getFile({
+				path: source.uri ?? "",
+				key: key,
+			});
+
+			return response;
+		},
+	});
+
+	return <Image {...props} source={{ uri: image.data?.url }} loading="lazy" />;
+}
