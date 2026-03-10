e5b69e7d15ff058090128d421c66df587e2802ad
do4mother <singgihputrap@gmail.com>
2026-03-10 14:35:48 +0700

    feat: enhance album query to include total photo count and associated gallery images

--- apps/backend/src/routers/album/find.ts
+++ apps/backend/src/routers/album/find.ts
@@ -1,22 +1,27 @@
 import { TRPCError } from "@trpc/server";
-import { and, eq, isNull } from "drizzle-orm";
+import { and, eq, getTableColumns, isNull, sql } from "drizzle-orm";
 import z from "zod";
-import { albums } from "../../db/schema";
+import { albums, gallery } from "../../db/schema";
 import { protectedProcdure } from "../../middlewares/protected";
 
 export const find = protectedProcdure
 	.input(z.object({ id: z.string() }))
 	.query(async ({ ctx, input }) => {
 		const [album] = await ctx.db
-			.select()
+			.select({
+				...getTableColumns(albums),
+				total: sql<number>`COUNT(${gallery.id})`,
+			})
 			.from(albums)
+			.leftJoin(gallery, eq(gallery.album_id, albums.id))
 			.where(
 				and(
 					eq(albums.id, input.id),
 					eq(albums.user_id, ctx.user.id),
 					isNull(albums.deleted_at),
 				),
-			);
+			)
+			.groupBy(albums.id);
 
 		if (!album) {
 			throw new TRPCError({
