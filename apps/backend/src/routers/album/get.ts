762141446524dbed2014d3e470f7ecf84318949c
do4mother <singgihputrap@gmail.com>
2026-03-11 11:23:09 +0700

    feat: enhance album retrieval with total photo count and gallery join

--- apps/backend/src/routers/album/get.ts
+++ apps/backend/src/routers/album/get.ts
@@ -1,10 +1,18 @@
-import { and, eq, isNull } from "drizzle-orm";
-import { albums } from "../../db/schema";
+import { and, eq, getTableColumns, isNull, sql } from "drizzle-orm";
+import { albums, gallery } from "../../db/schema";
 import { protectedProcdure } from "../../middlewares/protected";
 
 export const get = protectedProcdure.query(async ({ ctx }) => {
 	return ctx.db
-		.select()
+		.select({
+			...getTableColumns(albums),
+			total: sql<number>`COUNT (${gallery.id})`,
+		})
 		.from(albums)
-		.where(and(isNull(albums.deleted_at), eq(albums.user_id, ctx.user.id)));
+		.leftJoin(
+			gallery,
+			and(eq(gallery.album_id, albums.id), isNull(gallery.deleted_at)),
+		)
+		.where(and(isNull(albums.deleted_at), eq(albums.user_id, ctx.user.id)))
+		.groupBy(albums.id);
 });
